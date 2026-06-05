5d4792f4cf452e48423b3a39a47bf95853cb5e78
Tituz175 <tobititus01@gmail.com>
2026-06-05 09:07:43 -0600

    Project structure modified; display.py, dashboard.py, and logger.py scripts moved into computation directory from utils directory

---
+++ computation/logger.py
@@ -0,0 +1,27 @@
+import os
+import logging
+
+
+def setup_logger():
+
+    os.makedirs("logs", exist_ok=True)
+
+    logger = logging.getLogger("agentic_debugger")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    if not logger.handlers:
+
+        formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(message)s'
+            )
+        
+        file_handler = logging.FileHandler(
+            "logs/system.log"
+            )
+
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)
+
+    return logger
